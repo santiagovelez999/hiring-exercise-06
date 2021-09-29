841b3ffa6fc92296356aa1f85936caf5f7bb17bb
Santiago Vélez Pulgarin <santiago.velez@ceiba.com.co>
2021-09-29 18:03:41 -0500

    Se realiza metodos para calcular la desviacion estandar

--- hiring-exercise-06/src/main/java/com/scotiabankcolpatria/hiring/CreditRiskAssessment.java
+++ hiring-exercise-06/src/main/java/com/scotiabankcolpatria/hiring/CreditRiskAssessment.java
@@ -1,10 +1,16 @@
 package com.scotiabankcolpatria.hiring;
 
+import java.util.Arrays;
+
 public class CreditRiskAssessment {
 
     public double standardDeviation(int[] paymentDelays) {
         //TODO implement.
-        return -1;
+        final float averageData = calculateAverage(paymentDelays);
+        System.out.println("Promedio ( " + averageData + " ) ");
+        final double varianceData = calculateVariance(averageData, paymentDelays);
+        System.out.println("Varianza ( " + varianceData + " ) ");
+        return calculateRoot(varianceData);
     }
 
     public int paymentDelayMaxPeakIndex(int[] paymentDelays) {
@@ -16,4 +22,38 @@ public class CreditRiskAssessment {
         //TODO implement.
         return null;
     }
+
+
+    private float calculateAverage(int[] paymentDelays){
+        int sumInputData = 0;
+        for (int index:paymentDelays){
+            sumInputData = sumInputData + index;
+        }
+        System.out.println("Suma: ( " + sumInputData + " ) ");
+        return sumInputData / paymentDelays.length;
+    }
+
+    private double calculateVariance(double average, int[] paymentDelays){
+        double totalSumVariance = 0;
+        for (int index:paymentDelays){
+            double sumIndex = 0;
+            double elevationVariance = 0;
+            sumIndex = (index - average);
+            elevationVariance = Math.pow(sumIndex, 2);
+            totalSumVariance = totalSumVariance + elevationVariance;
+        }
+        return  totalSumVariance/paymentDelays.length;
+    }
+
+    private double calculateRoot(double variance){
+        return Math.sqrt(variance);
+    }
+
+
+    public static void main(String[] args){
+        CreditRiskAssessment riskAssessment = new CreditRiskAssessment();
+        double standardDeviation = riskAssessment.standardDeviation(new int[]{0, 15, 2, 0, 1, 3});
+        System.out.println("Desviación estándar: ( " + standardDeviation + " ) ");
+    }
+
 }
