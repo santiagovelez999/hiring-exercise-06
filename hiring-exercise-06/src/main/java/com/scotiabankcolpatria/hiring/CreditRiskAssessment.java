371357ad6a3b960430a67a365329e3cf50c61365
Santiago Vélez Pulgarin <santiago.velez@ceiba.com.co>
2021-09-29 20:55:36 -0500

    Se agrega metodos para calcular los valores de probabilidades de pagos tardios

--- hiring-exercise-06/src/main/java/com/scotiabankcolpatria/hiring/CreditRiskAssessment.java
+++ hiring-exercise-06/src/main/java/com/scotiabankcolpatria/hiring/CreditRiskAssessment.java
@@ -1,5 +1,9 @@
 package com.scotiabankcolpatria.hiring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CreditRiskAssessment {
 
     public double standardDeviation(int[] paymentDelays) {
@@ -22,10 +26,13 @@ public class CreditRiskAssessment {
     }
 
     public double[] latePaymentProbabilityByPeriod(int[][] paymentDelays) {
-        //TODO implement.
-        return null;
+        final List<Double> finalData = calculateProbabilityOfLateLayment(paymentDelays);
+        return convertListToVector(finalData);
     }
 
+    private double[] convertListToVector(List<Double> finalData){
+        return finalData.stream().mapToDouble(i -> i).toArray();
+    }
 
     private double calculateAverage(int[] paymentDelays){
         double sumInputData = 0;
@@ -51,12 +58,37 @@ public class CreditRiskAssessment {
         return Math.sqrt(variance);
     }
 
+    private List<Double> calculateProbabilityOfLateLayment(int[][] paymentDelays){
+        final int rowSize = paymentDelays.length;
+        final int columnSize = paymentDelays[0].length;
+        List<Double> finalData = new ArrayList<>();
+        int latePaymentsAmount = 0;
+        for(int columnIndex = 0; columnIndex < columnSize; columnIndex++){
+            for(int rowIndex = 0; rowIndex < rowSize; rowIndex++) {
+                if(paymentDelays[rowIndex][columnIndex] != 0){
+                    latePaymentsAmount++;
+                }
+                if(rowIndex == (rowSize - 1)){
+                    double dato = (double) latePaymentsAmount/rowSize;
+                    finalData.add(dato);
+                    latePaymentsAmount = 0;
+                }
+            }
+        }
+        return finalData;
+    }
 
     public static void main(String[] args){
         CreditRiskAssessment riskAssessment = new CreditRiskAssessment();
         //double standardDeviation = riskAssessment.standardDeviation(new int[]{-5, 1, 8, 7, 2});
-        int mayor = riskAssessment.paymentDelayMaxPeakIndex(new int[]{-5, 1, 8, 31, 7, 2});
-        System.out.println("Mayor: ( " + mayor + " ) ");
+        //int mayor = riskAssessment.paymentDelayMaxPeakIndex(new int[]{-5, 1, 8, 31, 7, 2});
+        double[] standardDeviation = riskAssessment.latePaymentProbabilityByPeriod(new int[][]{
+                {0, 3, 6, 1, 0, 5, 0, 0},
+                {0, 3, 0, 2, 0, 0, 0, 2},
+                {0, 0, 1, 0, 3, 0, 2, 0},
+                {0, 4, 0, 2, 0, 1, 1, 0}
+        });
+        Arrays.stream(standardDeviation).forEach(x->{System.out.print(" " + x);});
     }
 
 }
